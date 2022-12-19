72ade797806e0fa6f5c999a1e7bb1de11c412e15
Stenio <jstenio.rocha@gmail.com>
2022-12-19 19:56:34 -0300

    feat: the base

---
+++ americas-nuxt/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-screen flex justify-center items-center">
+    <h1 class="text-3xl font-bold text-center">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
