d4c2ab19759934451d368758f70d74577f83cb60
Stenio <jstenio.rocha@gmail.com>
2022-12-21 16:44:56 -0300

    feat: add github image

--- components/atoms/GithubLogo.vue
+++ components/atoms/GithubLogo.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <div class="github">
+    <a
+      href="https://github.com/stamorim28"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src="@/static/gh.png" alt="github" />
+    </a>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,3 +17,12 @@ export default Vue.extend({
   name: 'GithubLogo',
 })
 </script>
+
+<style lang="scss" scoped>
+.github {
+    position: fixed;
+    top: 2%;
+    right: 2%;
+    z-index: 2;
+  }
+</style>
