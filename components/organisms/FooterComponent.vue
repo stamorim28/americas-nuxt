a32012a0a2d565835a4c89925fdd006391d1b9b4
Stenio <jstenio.rocha@gmail.com>
2022-12-20 23:20:56 -0300

    feat: footer component

---
+++ components/organisms/FooterComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <ContainerComponent>
+    <footer class="flex justify-center items-center">
+      <h1 class="text-3xl font-bold text-center">
+        Boa noite
+      </h1>
+    </footer>
+</ContainerComponent>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'FooterComponent',
+})
+</script>
