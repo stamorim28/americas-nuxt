31c0d7c45e34c95871257f037ed6a75532e24376
Stenio <jstenio.rocha@gmail.com>
2022-12-20 23:18:53 -0300

    feat: layout of pages

---
+++ layouts/americas.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <HeaderComponent title="Americas"/>
+    <ContainerComponent>
+      <Nuxt/>
+    </ContainerComponent>
+    <FooterComponent/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'americas',
+})
+</script>
