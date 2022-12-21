a664e1ff7991f1af496be6b98654bf370b697120
Stenio <jstenio.rocha@gmail.com>
2022-12-21 16:44:01 -0300

    style: CountriesList style

--- components/organisms/CountriesList.vue
+++ components/organisms/CountriesList.vue
@@ -1,5 +1,8 @@
 <template>
-  <div></div>
+  <div class="countries-list grid grid-cols-5 grid-rows-auto gap-5">
+    <!-- <CountriesCard v-for="country in $countries" :key="country.id" :country ="country " /> -->
+    <CountriesCard />
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,3 +12,14 @@ export default Vue.extend({
   name: 'CountriesList',
 })
 </script>
+
+<style lang="scss" scoped>
+.countries-list {
+  @include screen('medium') {
+    @apply grid grid-cols-3
+  }
+  @include screen('small') {
+    @apply grid grid-cols-2
+  }
+}
+</style>
