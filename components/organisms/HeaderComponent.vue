0acc62bd0ecb44bd85459e121c7329a61bc976f6
Stenio <jstenio.rocha@gmail.com>
2022-12-20 23:20:46 -0300

    feat: header component

---
+++ components/organisms/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="flex justify-center items-center">
+    <h1 class="text-6xl font-bold text-center font-mono">
+      {{title}}
+    </h1>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'HeaderComponent',
+  props: {
+    title: {
+      type: String,
+      default: 'Americas'
+    }
+  }
+})
+</script>
