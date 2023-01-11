7d807be6df2d307248bc9faf62af5f69dbaf323d
Stenio <jstenio.rocha@gmail.com>
2023-01-11 18:19:58 -0300

    fix: fixing comma separation of languages ​​and timezones

--- components/organisms/CountryDetails.vue
+++ components/organisms/CountryDetails.vue
@@ -1,27 +1,42 @@
 <template>
   <section class="w-full">
     <div class="countries-details grid justify-items-center">
-      <nuxt-img class="w-full md:w-3/6" :src="country.flags.svg" :alt="country.name.common" loading="lazy" placeholder/>
-      <h2 class="text-2xl font-bold mt-4"> {{ country.name.official }}</h2>
+      <nuxt-img
+        class="w-full md:w-3/6"
+        :src="country.flags.svg"
+        :alt="country.name.common"
+        loading="lazy"
+        placeholder
+      />
+      <h2 class="text-2xl font-bold mt-4">{{ country.name.official }}</h2>
       <h4>{{ country.name.common }}</h4>
     </div>
 
     <div class="my-4">
-      <p>Languages:
-        <span v-for="(lang, idx) in getLanguages" :key="idx">{{ lang }}, </span>
+      <p>
+        Languages:
+        <span>{{ getLanguages }} </span>
       </p>
       <p>Subregion: {{ country.subregion }}</p>
       <p>Population: {{ getPopulation }} million</p>
-      <p>Timezones:
-        <span v-for="(timezones, idx) in country.timezones" :key="idx">
-          {{ timezones }}
+      <p>
+        Timezones:
+        <span>
+          {{ getTimezones }}
         </span>
       </p>
     </div>
 
     <div v-if="country.coatOfArms.png">
-      <h4 class="text-2xl font-bold">Coat of Arms:</h4> <br/>
-      <nuxt-img class="w-full md:w-1/4" :src="country.coatOfArms.png" :alt="`Coat of Arms: ${country.name.common}`" loading="lazy" placeholder/>
+      <h4 class="text-2xl font-bold">Coat of Arms:</h4>
+      <br />
+      <nuxt-img
+        class="w-full md:w-1/4"
+        :src="country.coatOfArms.png"
+        :alt="`Coat of Arms: ${country.name.common}`"
+        loading="lazy"
+        placeholder
+      />
     </div>
   </section>
 </template>
@@ -39,31 +54,41 @@ export default Vue.extend({
     } as PropOptions<Country>,
   },
 
-  head() : Object {
+  head(): Object {
     return {
       title: this.country.name.common,
       meta: [
         {
           hid: 'description',
           name: this.country.name.common,
-          content: this.country.name.official
-        }
+          content: this.country.name.official,
+        },
         // DESCRIÇÃO DAS METATAGS
-      ]
+      ],
     }
   },
 
   computed: {
-    getPopulation() : string {
-      const population : string = JSON.stringify(this.country.population / 1000000)
+    getPopulation(): string {
+      const population: string = JSON.stringify(
+        this.country.population / 1000000
+      )
       return population.substring(0, 5)
     },
 
-    getLanguages() : object {
-      const languages : object = this.country.languages
-      return Object.values(languages)
-    }
-  }
+    getLanguages(): string {
+      const languages: object = this.country.languages
+      const langsInArray: string[] = Object.values(languages)
+
+      return langsInArray.join(', ')
+    },
+
+    getTimezones(): string {
+      const timezones: string[] = this.country.timezones
+
+      return timezones.join(', ')
+    },
+  },
 })
 </script>
 <style lang="scss" scoped>
